ec46575f72fc5f1e0b1a9d1b0e3a5a545677efd1
020226 <mmin08@naver.com>
2025-02-01 15:09:58 +0900

    테스트 findAll

--- qna_service/src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
+++ qna_service/src/test/java/com/sbs/qna_service/QnaServiceApplicationTests.java
@@ -2,11 +2,15 @@ package com.sbs.qna_service;
 
 import com.sbs.qna_service.boundedContext.home.question.Question;
 import com.sbs.qna_service.boundedContext.home.question.QuestionRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class QnaServiceApplicationTests {
@@ -18,18 +22,30 @@ class QnaServiceApplicationTests {
 	private QuestionRepository questionRepository;
 
 	@Test
+	@DisplayName("데이터 저장")
 	void t1() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
+		this.questionRepository.save(q1); // insert 쿼리
 
 		Question q2 = new Question();
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q2);
+	}
+
+	@Test
+	@DisplayName("findAll")
+	void t2() {
+		List<Question> all = questionRepository.findAll();
+		// assertEquals(기댓값, 실젯값): 테스트에서 예상한 결과와 실제 결과가 동일한지 확인
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
